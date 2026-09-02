7e918173bfbb3f7ad2c36aee660788d573598724
powemetal <francisbvrt@gmail.com>
2026-09-02 10:25:52 -0400

    [DAO] fait le dao et le service bibliotheque pour recuperer les fichiers du cvs

--- src/main/java/com/maisonneuve/tp2_algorithme_spotify/model/ChansonDAO.java
+++ src/main/java/com/maisonneuve/tp2_algorithme_spotify/model/ChansonDAO.java
@@ -1,4 +1,42 @@
 package com.maisonneuve.tp2_algorithme_spotify.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChansonDAO {
+    public List<Chanson> charger(String chemin)  {
+        List<Chanson> chansons = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(chemin))) {
+            String ligne;
+
+            br.readLine();
+
+            while ((ligne = br.readLine()) != null) {
+                String[] donnees = ligne.split(",");
+
+                Chanson c = new Chanson(
+                        donnees[0],                      // Track URI → id
+                        donnees[1],                      // Track Name → titre
+                        donnees[2],                      // Artist Name(s) → artiste
+                        donnees[3],                      // Album Name → album
+                        donnees[8],                      // Artist Genres → genre
+                        donnees[10],                    // Label
+                        Integer.parseInt(donnees[4]), // Album Release Date → anneeSortie
+                        Integer.parseInt(donnees[6]),    // Track Duration (ms) → duree
+                        Integer.parseInt(donnees[7]),    // Popularity → nbrEcoute
+                        Float.parseFloat(donnees[9]),    // Danceability → dansabilitee
+                        donnees[5]                       // Album Image URL → imageUrl
+                );
+                chansons.add(c);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return chansons;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.maisonneuve.tp2_algorithme_spotify {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.sql;
 
     opens com.maisonneuve.tp2_algorithme_spotify to javafx.fxml;
     exports com.maisonneuve.tp2_algorithme_spotify;
