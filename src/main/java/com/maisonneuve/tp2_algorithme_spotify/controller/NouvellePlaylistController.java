1248a5a4686b2016fc1152e1d71dee228e203422
Mathieu Gosselin <math.gosselin@gmail.com>
2026-09-08 23:40:36 -0400

    feature: ajout de CSS dans Main et NouvellePlaylist

---
+++ src/main/java/com/maisonneuve/tp2_algorithme_spotify/controller/NouvellePlaylistController.java
@@ -0,0 +1,12 @@
+package com.maisonneuve.tp2_algorithme_spotify.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class NouvellePlaylistController {
+    @FXML
+    private TextField fieldNomNouvelleListe;
+}
